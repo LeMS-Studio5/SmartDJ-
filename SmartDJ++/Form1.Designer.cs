8f7445fc8026f3ba03fdc98c2a5bb49d97241065
Luke <Luke@WIN7-ASPIRE>
2022-04-13 01:09:52 -0400

    Add project files.

---
+++ SmartDJ++/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace SmartDJ__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.fbdMusic = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Title = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Album = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Artist = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Genre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Year = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "&Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // fbdMusic
+            // 
+            this.fbdMusic.RootFolder = System.Environment.SpecialFolder.DesktopDirectory;
+            this.fbdMusic.SelectedPath = "H:\\Music\\3-2-1 Penguins";
+            this.fbdMusic.ShowNewFolderButton = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Title,
+            this.Album,
+            this.Artist,
+            this.Genre,
+            this.Year});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 50);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(610, 361);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Title
+            // 
+            this.Title.Text = "Title";
+            // 
+            // Album
+            // 
+            this.Album.Text = "Album";
+            // 
+            // Artist
+            // 
+            this.Artist.Text = "Artist";
+            // 
+            // Genre
+            // 
+            this.Genre.Text = "Genre";
+            // 
+            // Year
+            // 
+            this.Year.Text = "Year";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(682, 50);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(610, 361);
+            this.listView2.TabIndex = 2;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Album";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Artist";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Genre";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Year";
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(666, 11);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare.TabIndex = 3;
+            this.btnCompare.Text = "&Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(27, 453);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1432, 506);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.FolderBrowserDialog fbdMusic;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Title;
+        private System.Windows.Forms.ColumnHeader Album;
+        private System.Windows.Forms.ColumnHeader Artist;
+        private System.Windows.Forms.ColumnHeader Genre;
+        private System.Windows.Forms.ColumnHeader Year;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ SmartDJ++/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using TagLib;
+namespace SmartDJ__
+{
+    public partial class Form1 : Form
+    {
+        //List<ReadID3v1Tags.ID3v1Tag> fileTags = new List<ReadID3v1Tags.ID3v1Tag>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            
+            if (fbdMusic.ShowDialog() == DialogResult.OK)
+            {
+                //ReadID3v1Tags i = new ReadID3v1Tags();
+                foreach(String fil in Directory.GetFiles(fbdMusic.SelectedPath,"*.mp3",SearchOption.AllDirectories))
+                {
+                    TagLib.File f = TagLib.File.Create(fil);
+                    ListViewItem l = new ListViewItem(f.Tag.Title);
+                        l.SubItems.Add(f.Tag.Album);
+                        l.SubItems.Add(f.Tag.AlbumArtists[0]);
+                        l.SubItems.Add(f.Tag.Genres[0]);
+                        l.SubItems.Add(f.Tag.Year.ToString());
+                        listView1.Items.Add(l);
+                    listView2.Items.Add((ListViewItem)l.Clone());
+                    //fileTags.Add(i.Read(fil));
+                }
+            }
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = CompareStrings(listView1.FocusedItem.Text, listView2.FocusedItem.Text).ToString();
+        }
+        private Int32 CompareStrings(String str1, String str2)
+        {
+            return 100-Math.Abs(string.Compare(str1, str2, System.Globalization.CultureInfo.CurrentCulture, System.Globalization.CompareOptions.OrdinalIgnoreCase));
+        }
+    }
+}
